e02fea9821113e32acd5d2e1f3ac38f3ac743939
Felipe Concha <felipeconcha@MacBook-Pro-de-Felipe.local>
2018-05-20 16:49:49 +0200

    test

--- R/leaflet_maps.R
+++ R/leaflet_maps.R
@@ -1,5 +1,10 @@
 install.packages("leaflet")
 library(leaflet)
+library(sp)
+
+data_sp <- SpatialPointsDataFrame(geolocalizacion2[,c(10, 11)]), data[]
 
 mapa_lea <- leaflet() %>% addTiles()
 mapa_lea
+
+

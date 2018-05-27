a07b9454e6e89856f2b5cfe68cc21bd1d6c9c939
Felipe Concha <felipeconcha@MacBook-Pro-de-Felipe.local>
2018-05-27 18:50:51 +0200

    Se suben los últimos cambios que contienen las dos pestañas con los mapas principales.

--- R/conexión a api.R
+++ R/conexión a api.R
@@ -1,3 +1,11 @@
 http://api.ipstack.com/147.83.201.105?access_key=ff6b408f4ce1e51e445323ecd2ce0688
 
 fromJSON(getURLContent("http://api.ipstack.com/147.83.201.105?access_key=ff6b408f4ce1e51e445323ecd2ce0688"))
+
+
+if (!require(devtools))
+  install.packages("devtools")
+devtools::install_github("rstudio/leaflet")
+shiny::runGitHub("rstudio/shiny-examples", subdir="063-superzip-example")
+
+
--- R/pruebashiny.R
+++ R/pruebashiny.R
@@ -1,15 +1,29 @@
 library(shiny)
 
 ui <- fluidPage(
-  h5("este es mi título"),
-  leafletOutput(
-    "mimapa", height = "600")
-)
+  tabsetPanel(
+    tabPanel("Muestra Ataques 48 horas", verbatimTextOutput("summary"), leafletOutput(
+      "mimapa", width = "100%", height = 910)
+    ), 
+    tabPanel("Total de ataques", tableOutput("table"), leafletOutput(
+      "world_map", width = "100%", height = 910) )
+    ),
+  
+  absolutePanel(
+    bottom = 50, right = 50, width = 200,
+    draggable = TRUE)
+  )
+ 
 
 server <- function(input, output, session) {
   output$mimapa <- renderLeaflet(leaflet() %>% addTiles() %>% 
                                    addMarkers(data = ip_geo_merge, lng = ip_geo_merge$longitude, lat = ip_geo_merge$latitude, popup = ip_geo_merge$servicio, clusterOptions = markerClusterOptions())
   )
+  output$world_map <-renderLeaflet( leaflet() %>% addTiles() %>% 
+    addMarkers(data = total_ataques, lng = total_ataques$longitude, lat = total_ataques$latitude, popup = total_ataques$time_zone, clusterOptions = markerClusterOptions())
+  )
 }
 
 shinyApp(ui, server)
+
+
